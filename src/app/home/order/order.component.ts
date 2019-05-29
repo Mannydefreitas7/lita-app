b68ca05fd3ad6c8cf9652ec11f416218dedf6816
Manuel <manuel@usd25xv7r2j1gq.bethel.jw.org>
2019-05-29 17:03:57 -0400

    updates

--- src/app/home/order/order.component.ts
+++ src/app/home/order/order.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AuthService } from 'src/app/core/auth.service';
 import { MatButtonToggleChange, MatTableDataSource, MatMonthView, MatSelectChange } from '@angular/material';
 import { DashboardService } from '../dashboard/dashboard.service';
 import { OrderService } from './order.service';
-import { map} from 'rxjs/operators';
+import { map, publish} from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { CongLiterature, Literature } from 'src/app/shared/models/congregation.model';
 import { AngularFirestore } from 'angularfire2/firestore';
@@ -13,14 +13,14 @@ import { AngularFirestore } from 'angularfire2/firestore';
   templateUrl: './order.component.html',
   styleUrls: ['../publishers/scss/publishers.component.scss']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, AfterViewInit {
   loading: boolean;
-  toggle: boolean  = true;
+  toggle: boolean = true;
   publications:any = [];
   currentMonth: number;
   month: number;
   dataSource: any;
-  searchText: string = "";
+  searchText: string = '';
   displayedColumns: string[] = ['id', 'name', 'pubId', 'contextTitle', 'quantityIn', 'quantityOnHand', 'quantityOut'];
   congLiteratures: Observable<any>
   literature: Observable<any>;
@@ -38,34 +38,15 @@ export class OrderComponent implements OnInit {
     this.loading = true;
     const date = new Date()
     this.currentMonth = date.getMonth();
-    // this.afs.collection<Literature>('literature').valueChanges().subscribe(lits => {
-
-    //     this.auth.user.subscribe(user => {
-    //     lits.forEach(lit => {
-    //     this.dash.getCongregationDoc(user.congregation).collection(`${5}`).doc<CongLiterature>(`${lit.id}`).valueChanges()
-    //       .pipe(map(pubs => { 
-    //              return {
-    //              id: pubs.id,
-    //              cover: lit.cover,
-    //              contextTitle: lit.contextTitle,
-    //              name: lit.name,
-    //              pubId: lit.pubId,
-    //              in: pubs.months[this.month].in,
-    //              onHand: pubs.months[this.month].onHand,
-    //              out: pubs.months[this.month].out
-    //            }
-    //         })).subscribe(data => {
-    //           this.publications.push(data)
-    //           this.dataSource = new MatTableDataSource(this.publications)
-    //           this.loading = false;
-    //           })
-    //       })
-    //     })
-    // })
     this.month = this.currentMonth;
+    setTimeout(() => {
+      this.showInventory(this.currentMonth)
+    }, 1000)
+  }
+  
 
-    this.showInventory(this.currentMonth)
-
+  ngAfterViewInit() {
+  // this.showInventory(this.month).unsubscribe();
   }
 
   applyFilter(filterValue: string) {
@@ -74,13 +55,17 @@ export class OrderComponent implements OnInit {
     }
   }
 
-  showInventory(selected: number) {
-    this.afs.collection<Literature>('literature').valueChanges().subscribe(lits => {
+ mapInventory(congID: number, litID) {
+    return this.dash.getCongregationDoc(congID).collection(`${5}`).doc<CongLiterature>(`${litID}`).valueChanges()
+  }
+
+showInventory(selected: number) {
+ this.afs.collection<Literature>('literature').valueChanges().subscribe(lits => {
       this.auth.user.subscribe(user => {
       lits.forEach(lit => {
-      this.dash.getCongregationDoc(user.congregation).collection(`${5}`).doc<CongLiterature>(`${lit.id}`).valueChanges()
+      return this.mapInventory(user.congregation, `${lit.id}`)
         .pipe(map(pubs => { 
-               return {
+             return {
                id: pubs.id,
                cover: lit.cover,
                contextTitle: lit.contextTitle,
@@ -92,19 +77,23 @@ export class OrderComponent implements OnInit {
              }
           })).subscribe(data => {
             this.publications.push(data)
-            this.dataSource = new MatTableDataSource(this.publications)
+            this.dataSource = new MatTableDataSource(this.publications);
             this.loading = false;
-            })
+          })
         })
       })
   })
-  }
 
-  monthDisplay(event: MatSelectChange) {
+}
+
+monthDisplay(event: MatSelectChange) {
+    this.publications = [];
     this.loading = true;
-    this.showInventory(event.value);
     console.log(event.value)
-  }
+    setTimeout(() => {
+      this.showInventory(event.value) 
+    }, 3000)
+}
 
   toggleView(change: MatButtonToggleChange) {
     this.toggle = change.value;
